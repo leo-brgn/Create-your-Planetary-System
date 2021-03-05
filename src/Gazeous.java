e99b10d642370a108437b0d6e7fc530f1b1db63f
roudoudou2000 <aliice_31@hotmail.fr>
2021-03-05 18:02:00 +0100

    public to private for method toString

---
+++ src/Gazeous.java
@@ -0,0 +1,2 @@
+public class Gazeous {
+}
--- src/Telluric.java
+++ src/Telluric.java
@@ -1,3 +1,19 @@
+import java.awt.*;
+
 public class Telluric extends Planet {
 
+    public Telluric () {
+    }
+
+    public String toString(){
+        return null;
+    }
+
+    public void computeMass() {
+
+    }
+
+    public void paint(Graphics g){
+
+    }
 }
