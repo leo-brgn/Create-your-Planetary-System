3b599b22278f7a43d2329c467ef54149fe0845b8
leo100bis <leo.brignone@insa-lyon.fr>
2021-03-05 17:04:07 +0100

    Added planet class

---
+++ src/Planet.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public abstract class Planet {
+    protected Color[6] colorRange;
+    protected float mass;
+    protected int radius;
+    protected float distanceToStar;
+    final public float G = 6.67430 * 10-11;
+
+
+}
