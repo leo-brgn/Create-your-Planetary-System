fb462b515acf2a63209ff0198180c05de510fb3b
Lisa-clbrn <lisa.colombarini@insa-lyon.fr>
2021-03-05 16:35:42 +0100

    Start the first panel

--- src/Title.java
+++ src/Title.java
@@ -1,2 +1,31 @@
-public class Title {
+import javax.swing.*;
+
+public class Title extends JFrame{
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public Title() {
+
+        JFrame startingFrame = new JFrame();
+        startingFrame.setTitle("Create my Solar System");
+        startingFrame.setSize(300,200);
+        startingFrame.setResizable(false);
+        startingFrame.setLocation(100,200);
+        startingFrame.setVisible(true);
+        startingFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+        JButton launchButton = new JButton("Start");
+
+
+        JTextField nbPlanetLabel= new JTextField();
+    }
+    private void ActionPerformed(){
+
+    }
 }
