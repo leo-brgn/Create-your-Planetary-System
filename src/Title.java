99a24dd992105779b4b9cd6311731bca2e4a4099
Lisa-clbrn <lisa.colombarini@insa-lyon.fr>
2021-03-06 11:54:35 +0100

    I add the "minus" button

--- src/Title.java
+++ src/Title.java
@@ -23,8 +23,8 @@ public class Title extends JFrame{
         firstPanel.setVisible(true);
 
         firstPanel.setLayout(null);
-        JLabel affBackground = new JLabel (new ImageIcon("wallpaper-HD.jpg"));
-        affBackground.setBounds(0,25,1050,640);
+        JLabel affBackground = new JLabel (new ImageIcon("items/wallpaper-HD.jpg"));
+        affBackground.setBounds(0,0,1050,640);
         firstPanel.add(affBackground);
 
         JLabel txtLabel = new JLabel();
@@ -53,6 +53,16 @@ public class Title extends JFrame{
         launchButton.setForeground(Color.WHITE);
         launchButton.setBackground(Color.GRAY);
         affBackground.add(launchButton);
+
+        JButton plusButton = new JButton(new ImageIcon("items/logo-plus.png"));
+        plusButton.setBounds(585,500,25,25);
+        // plusButton.setForeground(Color.WHITE);
+        affBackground.add(plusButton);
+
+        JButton minusButton = new JButton(new ImageIcon("items/logo-minus.png"));
+        minusButton.setBounds(440,500,25,25);
+        // plusButton.setForeground(Color.WHITE);
+        affBackground.add(minusButton);
 
         this.add(firstPanel);
         this.setVisible(true);
