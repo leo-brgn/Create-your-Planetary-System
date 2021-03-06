4c7dae053525187cc69c0b5baa90aa3fbaa02c91
leo100bis <leo.brignone@insa-lyon.fr>
2021-03-06 17:32:33 +0100

    Adding MouseListener

--- src/CelestialObject.java
+++ src/CelestialObject.java
@@ -13,12 +13,11 @@ public abstract class CelestialObject extends JComponent implements Comparable<C
     final public float G = (float) (6.67430 * Math.pow(10, -11));
 
     protected float[] initialVelocity = new float[2];
-    protected int[] position = new int[2];
+    protected Point position;
 
     /**
      * Abstract methods
      */
-
     public abstract String toString();
     public abstract void computeMass();
 
--- src/Window.java
+++ src/Window.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.EventListener;
+import java.util.Vector;
 
-public class Window extends JFrame {
-    public int[] dimensions;
+public class Window extends JFrame implements MouseListener {
+    // GUI attributes
     public JPanel rightPanel;
     public JLabel nbPlanet;
     public JButton telluric;
@@ -12,25 +16,50 @@ public class Window extends JFrame {
     public JLabel  color;
     public JLabel preview;
     public JButton validate;
+    public Point mouseLocation;
+    // System attributes
     public int nbPlanets;
     public PlanetarySystem planetarySystem;
 
     public Window(int p){
         nbPlanets = p;
         planetarySystem = new PlanetarySystem();
-
+        // Creation of the window
         this.setTitle("Create your Solar System");
         this.setSize(1050,640);
         this.setResizable(false);
         this.setLocation(120,20);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
-        this.setBackground(Color.BLACK);
+        this.setBackground(Color.BLUE);
         this.setVisible(true);
+        this.addMouseListener(this);
+        // Creation of the panel
     }
 
-    public void addObjectToSystem(CelestialObject co){
-        planetarySystem.addCelestialObj(co);
+    /**
+     * Method to handle the action on the mouse
+     */
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+
     }
 
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+        if(mouseEvent.getButton() == MouseEvent.BUTTON1){
+            System.out.println(mouseEvent.getPoint());
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {}
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {}
 }
