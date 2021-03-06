72b29e912aec9768c99d1b7e488eb3be79b9a73b
Lisa-clbrn <lisa.colombarini@insa-lyon.fr>
2021-03-06 12:01:21 +0100

    I add the "minus" button

--- src/Title.java
+++ src/Title.java
@@ -31,7 +31,7 @@ public class Title extends JFrame{
         txtLabel.setText("CREATE YOUR PLANETARY SYSTEM");
         txtLabel.setFont(new java.awt.Font(Font.SERIF,Font.BOLD,45));
         txtLabel.setForeground(Color.WHITE);
-        txtLabel.setBounds(110,25,830,50);
+        txtLabel.setBounds(110,35,830,50);
         affBackground.add(txtLabel);
 
         JLabel titleLabel = new JLabel();
