b4377d11d9276a6778b2dafbdb1be65d5aa2f83e
Lisa-clbrn <lisa.colombarini@insa-lyon.fr>
2021-03-06 11:23:23 +0100

    I'm changing the backgroung picture

--- src/Title.java
+++ src/Title.java
@@ -23,33 +23,33 @@ public class Title extends JFrame{
         firstPanel.setVisible(true);
 
         firstPanel.setLayout(null);
-        JLabel affBackground = new JLabel (new ImageIcon("wallpaper HD.jpg"));
-        affBackground.setBounds(0,25,1050,590);
+        JLabel affBackground = new JLabel (new ImageIcon("wallpaper-HD.jpg"));
+        affBackground.setBounds(0,25,1050,640);
         firstPanel.add(affBackground);
 
         JLabel txtLabel = new JLabel();
         txtLabel.setText("CREATE YOUR PLANETARY SYSTEM");
         txtLabel.setFont(new java.awt.Font(Font.SERIF,Font.BOLD,45));
         txtLabel.setForeground(Color.WHITE);
-        txtLabel.setBounds(90,25,900,50);
+        txtLabel.setBounds(110,25,830,50);
         affBackground.add(txtLabel);
 
         JLabel titleLabel = new JLabel();
         titleLabel.setText("How many planet do you want to add ?");
         titleLabel.setFont(new java.awt.Font(Font.SERIF,Font.BOLD,30));
         titleLabel.setForeground(Color.WHITE);
-        titleLabel.setBounds(295,425,600,50);
+        titleLabel.setBounds(270,425,510,50);
         affBackground.add(titleLabel);
 
         JTextField nbPlanetLabel= new JTextField();
-        nbPlanetLabel.setBounds(495,495,100,35);
+        nbPlanetLabel.setBounds(475,495,100,35);
         nbPlanetLabel.setHorizontalAlignment(SwingConstants.CENTER);
         nbPlanetLabel.setBackground(Color.BLACK);
         nbPlanetLabel.setForeground(Color.WHITE);
         affBackground.add(nbPlanetLabel);
 
         JButton launchButton = new JButton("Start");
-        launchButton.setBounds(505,535,80,25);
+        launchButton.setBounds(485,535,80,25);
         launchButton.setForeground(Color.WHITE);
         launchButton.setBackground(Color.GRAY);
         affBackground.add(launchButton);
