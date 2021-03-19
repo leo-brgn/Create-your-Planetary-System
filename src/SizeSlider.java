218d3da9dd5f821ff80a2b252840282352d5b6b0
maepuggia <mae.puggia@gmail.com>
2021-03-19 18:58:36 +0100

    unhhh trying to create a slider

--- src/SizeSlider.java
+++ src/SizeSlider.java
@@ -1,22 +1,21 @@
-public class SizeSlider {
-    import javax.swing.*;
+import javax.swing.*;
 
-    public class SizeSlider extends JFrame{
-        public SizeSlider() {
-            JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 50, 25);
-            slider.setMinorTickSpacing(2);
-            slider.setMajorTickSpacing(10);
-            slider.setPaintTicks(true);
-            slider.setPaintLabels(true);
+public class SizeSlider extends JFrame{
+    public SizeSlider() {
+        JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 50, 25);
+        slider.setMinorTickSpacing(2);
+        slider.setMajorTickSpacing(10);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
 
-            JPanel panel=new JPanel();
-            panel.add(slider);
-            add(panel);
-        }
-        public static void main(String s[]) {
-            SizeSlider frame=new SizeSlider();
-            frame.pack();
-            frame.setVisible(true);
-        }
+        JPanel panel=new JPanel();
+        panel.add(slider);
+        add(panel);
+    }
+    public static void main(String[] s) {
+        SizeSlider frame = new SizeSlider();
+        frame.pack();
+        frame.setVisible(true);
     }
 }
+
