6d268a1db02f6ac21942ebd8512b0d4dd0bd6bdf
maepuggia <mae.puggia@gmail.com>
2021-03-19 19:32:28 +0100

    unhhh trying to create a slider

--- src/Window.java
+++ src/Window.java
@@ -80,11 +80,6 @@ public class Window extends JFrame implements ActionListener, MouseListener {
         backPlanetNb.setBounds(0,25,270,60);
         rightPanel.add(backPlanetNb);
 
-        slider = new JPanel();
-        slider.setBackground(new Color(51,48,51));
-        backPlanetNb.setBounds(100,170,150,50);
-        rightPanel.add(slider);
-
 
         planetNb = new JLabel();
         planetNb.setText("PLANET: " + currentPlanet + "/" + nbPlanets);
