1540de5d84ea8e96a82414cbf4c882e98abe8abe
maepuggia <mae.puggia@gmail.com>
2021-03-19 17:20:12 +0100

    adding colors

--- src/Window.java
+++ src/Window.java
@@ -24,17 +24,17 @@ public class Window extends JFrame implements MouseListener {
     private Color[] listColor={
             new Color(195, 155, 211),
             new Color(187, 143, 206),
-            new Color(),
-            new Color(),
-            new Color(),
-            new Color(),
+            new Color(127, 179, 213),
+            new Color(133, 193, 233),
+            new Color(115, 198, 182),
+            new Color(130, 224, 170),
 
-            new Color(),
-            new Color(),
-            new Color(),
-            new Color(),
-            new Color(),
-            new Color(),
+            new Color(247, 220, 111),
+            new Color(245, 176, 65),
+            new Color(243, 156, 18),
+            new Color(230, 126, 34),
+            new Color(231, 76, 60),
+            new Color(192, 57, 43),
     }
 
     public Window(int nbPlanets){
