d681ef8a57469d3cfd07bcc62a0bed451da30676
leo100bis <leo.brignone@gmail.com>
2021-04-09 18:12:49 +0200

    Collisions

--- src/Star.java
+++ src/Star.java
@@ -25,6 +25,7 @@ public class Star extends CelestialObject{
     public Star(int rad){
         super(rad, new Point(390,320));
         alpha = 1.0f;
+        radiusKm = radius * scaleSunSize;
         typeStr = "Star";
         density = 1.41;//same as our central star
         objectName = "Sun";
@@ -38,9 +39,8 @@ public class Star extends CelestialObject{
         float[] dist = {0.05f*alpha, 0.1f*alpha, 0.17f*alpha, 0.22f * alpha, 0.35f*alpha, 1*alpha};
         RadialGradientPaint p = new RadialGradientPaint(position, radius, dist, colors);
         g2D.setPaint(p);
-        g2D.fillOval(position.x-radius-15,position.y-radius-15,2*radius + 30,2*radius + 30);
+        g2D.fillOval(position.x-3*radius-15,position.y-3*radius-15,3*2*radius + 30,3*2*radius + 30);
     }
-
 
     public void updateSun(float deltaT2){
         alpha = (float) Math.abs(Math.cos(deltaT2));
